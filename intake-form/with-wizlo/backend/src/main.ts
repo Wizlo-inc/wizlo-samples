bf88840ae424e67c78a08fc0eec0ed1d7b2a0008
Anshika Shrivastava <anshika.shrivastava@techdome.net.in>
2026-05-19 18:34:59 +0530

    vouched-verification

--- intake-form/with-wizlo/backend/src/main.ts
+++ intake-form/with-wizlo/backend/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   const port = process.env.PORT || 3003;
